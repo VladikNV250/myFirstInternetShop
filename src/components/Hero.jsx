ac84c7b8b57b3906844d48d8bfd640fa6e0e28ba
VladikNV250 <vladiknemichev@gmail.com>
2022-06-07 19:07:13 +0300

    styled hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,28 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
  
 const Hero = ({img}) => {
   return (
-    <section>
-        <img src={img} alt="nema" style={{zIndex: '0'}}/>
-          <FAButton icon={faChevronLeft} color="grey" />
-          <FAButton icon={faChevronRight} color="grey" />
-          <h1>Ababa laga ma</h1>
+    <section className='hero__section'>
+        <img className='hero__bg' src={img} alt="nema" />
+          <div className='hero__btn'>
+            <FAButton 
+              icon={faChevronLeft} 
+              color="white" 
+              size='67' 
+              iColor='#cdcdcd'
+            />
+          </div>
+          <div className='hero__header'>
+            <h1>Aluminium Club</h1>
+            <h1 style={{fontWeight: '700'}}>Experience Ray-Ban</h1>
+          </div>
+          <div className='hero__btn' style={{left: '86%'}}>
+          <FAButton 
+            icon={faChevronRight} 
+            color="white" 
+            size='67' 
+            iColor='#cdcdcd'
+          />
+          </div>
     </section>
   )
 }
--- src/components/UI/button/FAButton.jsx
+++ src/components/UI/button/FAButton.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import cl from './FAButton.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
  
-const FAButton = ({icon, color}) => {
+const FAButton = ({icon, color, size, iColor}) => {
 
   return (
-    <button className={cl.faButton} style={{background: color}}><FontAwesomeIcon icon={icon} /></button>
+    <button 
+      className={cl.faButton} 
+      style={{background: color, width: size + 'px', height: size + 'px'}}
+    >
+      <FontAwesomeIcon 
+        style={{fontSize: size / 2.3, color: iColor}} 
+        icon={icon} 
+      />
+    </button>
   )
 }
  
