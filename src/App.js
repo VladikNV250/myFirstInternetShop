760ea4644eda3bebb02b16cbf3ebc4cc170aa9bc
VladikNV250 <vladiknemichev@gmail.com>
2022-06-10 23:32:30 +0300

    styled goods list

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ import GoodsList from './components/GoodsList';
 
 function App() {
   const [goods, setGoods] = useState([
-    { title: 'окуляри', price: '400', img: image.glasses },
-    { title: 'окуляри1', price: '500', img: image.glasses },
-    { title: 'окуляри2', price: '250', img: image.glasses },
-
+    { title: 'окуляри зручні lorem ipsum для дітей asdasda dasdads adad asda dsad',  price: '400.00', img: image.glasses },
+    { title: 'окуляри1', price: '500.00', img: image.glasses },
+    { title: 'окуляри2', price: '250.20', img: image.glasses },
+    { title: 'окуляри3', price: '400.00', img: image.glasses },
+    { title: 'окуляри4', price: '500.20', img: image.glasses },
+    { title: 'окуляри5', price: '250.20', img: image.glasses },
+    { title: 'окуляр6',  price: '400.20', img: image.glasses },
+    { title: 'окуляри7', price: '500.00', img: image.glasses },
+    { title: 'окуляри8', price: '250.20', img: image.glasses },
   ])
 
   return (
@@ -20,18 +25,18 @@ function App() {
       <Navbar />
       <Hero img={image.hero1} />
       <SideBar />
-      <GoodsList 
-        title="Улюблені товари"
-        goods={goods}
-      />
-      <GoodsList 
-        title="Рекомендовано"
-        goods={goods}
-      />
-      <GoodsList 
-        title="Нові товари"
-        goods={goods}
-      />
+        <GoodsList 
+          title="Улюблені товари"
+          goods={goods}
+        />
+        <GoodsList 
+          title="Рекомендовано"
+          goods={goods}
+        />
+        <GoodsList 
+          title="Нові товари"
+          goods={goods}
+        />
       <footer>
         
       </footer>
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -3,14 +3,33 @@ import '../css/Tile.css';
 import FAButton from './UI/button/FAButton';
 import { faBasketShopping } from '@fortawesome/free-solid-svg-icons';
  
-const Tile = ({title, price, img}) => {
+const Tile = ({title, price, img, currency}) => {
+  let curentCurrency = '$'
+
+  switch(currency) {
+    case 'USD': curentCurrency = '$' ; break
+    case 'GRN': curentCurrency = '₴' ; break
+    case 'GBP': curentCurrency = '£' ; break
+    case 'PLN': curentCurrency = 'zł' ; break
+    case 'EUR': curentCurrency = '€' ; break
+  }
+
+
   return (
-    <li>
-        <article>
-            <img src={img} alt="" />
-            <h3>{title}</h3>
-            <h3>{price}</h3>
-            <FAButton icon={faBasketShopping} color="#7db122" />
+    <li className='tile'>
+        <article className='inner__tile'>
+          <div className='tile__image'>
+            <img  src={img} alt={title} />
+          </div>
+          <div className='tile__body'>
+            <div>
+              <h3 className='tile__name'>{title}</h3>
+              <h3 className='tile__price'>{price} {curentCurrency}</h3>
+            </div>
+            <a className='tile__btn'>
+              <FAButton icon={faBasketShopping} color="#7db122" />
+            </a>
+          </div>
         </article>
     </li>
   )
